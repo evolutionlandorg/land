15362756a74d364f41404238e38e1eaa4be28b35
HackFisher <hackfisher@gmail.com>
2018-06-21 13:11:47 +0800

    add init contracts

---
+++ contracts/upgradable/IApplication.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.4.23;
+
+contract IApplication {
+  function initialize(bytes data) public;
+}
---
+++ contracts/upgradable/Proxy.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.4.23;
+
+import "../Storage.sol";
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import "./DelegateProxy.sol";
+import "./IApplication.sol";
+
+contract Proxy is Storage, DelegateProxy, Ownable {
+
+  event Upgrade(address indexed newContract, bytes initializedWith);
+  event OwnerUpdate(address _prevOwner, address _newOwner);
+
+  constructor() public {
+    proxyOwner = msg.sender;
+    owner = msg.sender;
+  }
+
+  //
+  // Ownership
+  //
+  modifier onlyProxyOwner() {
+    require(msg.sender == proxyOwner);
+    _;
+  }
+
+  function transferOwnership(address _newOwner) public onlyProxyOwner {
+    require(_newOwner != address(0));
+    require(_newOwner != proxyOwner);
+    proxyOwner = _newOwner;
+  }
+
+  //
+  // Upgrade
+  //
+
+  function upgrade(IApplication newContract, bytes data) public onlyProxyOwner {
+    currentContract = newContract;
+    IApplication(this).initialize(data);
+
+    Upgrade(newContract, data);
+  }
+
+  //
+  // Dispatch fallback
+  //
+
+  function () payable public {
+    require(currentContract != 0); // if app code hasn't been set yet, don't call
+    delegatedFwd(currentContract, msg.data);
+  }
+}
