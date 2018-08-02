92d48b13a0beee49f6874cd22268f18a152542ba
董淑伟 <dsw0602@foxmail.com>
2018-08-02 17:46:48 +0800

    add LANDdata.sol and modify the structure of the project

---
+++ contracts/land/LANDdata.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./LANDbase.sol";
+
+contract LANDdata is Ownable, LANDbase{
+
+    struct LandPixel {
+        int x;  // position on the x-axis
+        int y;  // position on the y-axis
+        int z;  // position on the z-axis
+        uint64 goldRate; // maximum of gold's generation per pixel and per day
+        uint64 woodRate;
+        uint64 waterRate;
+        uint64 fireRate;
+        uint64 soilRate;
+        bool isReserved; // is this land reserved. it means not for sale.
+    }
+
+    mapping(uint256 => LandPixel) tokenId2LandPixel;
+
+    function addLandPixel(
+        uint256 _tokenId,
+        int _x,
+        int _y,
+        int _z,
+        uint64 _goldRate,
+        uint64 _woodRate,
+        uint64 _waterRate,
+        uint64 _fireRate,
+        uint64 _soilRate,
+        bool _isReserved)
+    public onlyOwner {
+        LandPixel memory landPixel = LandPixel(_x,_y,_z,_goldRate,_woodRate,_waterRate,_fireRate,_soilRate,_isReserved);
+        uint tokenId = _encodeTokenId(_x,_y);
+        tokenId2LandPixel[tokenId] = landPixel;
+    }
+
+    function getPixelResources(int _x, int _y) public view returns (uint64,uint64,uint64,uint64,uint64,bool) {
+        uint256 tokenId = _encodeTokenId(_x, _y);
+        LandPixel landPixel = tokenId2LandPixel[tokenId];
+        return (landPixel.goldRate,
+                landPixel.woodRate,
+                landPixel.waterRate,
+                landPixel.fireRate,
+                landPixel.soilRate,
+                landPixel.isReserved);
+    }
+
+}
