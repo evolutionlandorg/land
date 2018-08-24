729708affe45636bd8ea8045c97767b68b51028e
董淑伟 <dsw0602@foxmail.com>
2018-08-24 14:49:48 +0800

    modify landInfo generation rule

--- contracts/land/LandData.sol
+++ contracts/land/LandData.sol
@@ -4,10 +4,6 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract LandData is Ownable {
 
-    uint256 constant CLEAR_LOW = 0xffff0000;
-    uint256 constant CLEAR_HIGH = 0x0000ffff;
-    uint256 constant FACTOR = 0x10000;
-
     // address of rewardBox
     address public rewardBox;
 
@@ -16,18 +12,13 @@ contract LandData is Ownable {
      * consider LandInfo a binary array with the index starting at 0.
      * the rightmost one is the 0th element and the leftmost one is 255th element.
      * from the right to the left:
-     * LandInfo[0,15] : y
-     * LandInfo[16,31] : x
-     * LandInfo[32,47] : z
-     * LandInfo[48,63] : goldrate
-     * LandInfo[64,79] : woodrate
-     * LandInfo[80,95] : waterrate
-     * LandInfo[96,111] : firerate
-     * LandInfo[112,127] : soilrate
-     * LandInfo[128,128] : isReserved
-     * LandInfo[129,129] : isSpecial
-     * LandInfo[130,130] : hasBox
-     * LandInfo[131,255] : not open yet
+     * LandInfo[0,15] : goldrate
+     * LandInfo[16,31] : woodrate
+     * LandInfo[32,47] : waterrate
+     * LandInfo[48,63] : firerate
+     * LandInfo[64,79] : soilrate
+     * LandInfo[80,95] : flag
+     * LandInfo[96,255] : not open yet
     */
     //uint256 LandInfo;
 
@@ -49,13 +40,6 @@ contract LandData is Ownable {
         }
     }
 
-    function getXY(uint _tokenId) public view returns (int16 x, int16 y) {
-        uint landInfo = tokenId2Attributes[_tokenId];
-        // get x and y
-        uint position = getInfoFromAttibutes(landInfo, 0, 31);
-        (x, y) = _decodeTokenId(position);
-    }
-
 
     function modifyAttibutes(uint _tokenId, uint _right, uint _left, uint _newValue) public {
         // unboxing will change resources on each land
@@ -75,61 +59,14 @@ contract LandData is Ownable {
         uint waterRate,
         uint fireRate,
         uint soilRate,
-        bool isReserved,
-        bool isSpecial,
-        bool hasBox) {
+        uint flag) {
         uint landInfo = tokenId2Attributes[_tokenId];
-        goldRate = getInfoFromAttibutes(landInfo, 48, 63);
-        woodRate = getInfoFromAttibutes(landInfo, 64, 79);
-        waterRate = getInfoFromAttibutes(landInfo, 80, 95);
-        fireRate = getInfoFromAttibutes(landInfo, 96, 111);
-        soilRate = getInfoFromAttibutes(landInfo, 112, 127);
-        isReserved = getInfoFromAttibutes(landInfo, 128, 128) == 1;
-        isSpecial = getInfoFromAttibutes(landInfo, 129, 129) == 1;
-        hasBox = getInfoFromAttibutes(landInfo, 130, 130) == 1;
-
-    }
-
-
-    function getGoldRate(uint _tokenId) public view returns (uint) {
-        uint landInfo = tokenId2Attributes[_tokenId];
-        return (getInfoFromAttibutes(landInfo, 48, 63));
-    }
-
-    function getWoodRate(uint _tokenId) public view returns (uint) {
-        uint landInfo = tokenId2Attributes[_tokenId];
-        return (getInfoFromAttibutes(landInfo, 64, 79));
-    }
-
-    function getWaterRate(uint _tokenId) public view returns (uint) {
-        uint landInfo = tokenId2Attributes[_tokenId];
-        return (getInfoFromAttibutes(landInfo, 80, 95));
-    }
-
-    function getFireRate(uint _tokenId) public view returns (uint) {
-        uint landInfo = tokenId2Attributes[_tokenId];
-        return (getInfoFromAttibutes(landInfo, 96, 111));
-    }
-
-    function getSoilRate(uint _tokenId) public view returns (uint) {
-        uint landInfo = tokenId2Attributes[_tokenId];
-        return (getInfoFromAttibutes(landInfo, 112, 127));
-    }
-
-
-    function isReserved(uint256 _tokenId) public view returns (bool) {
-        uint landInfo = tokenId2Attributes[_tokenId];
-        return (getInfoFromAttibutes(landInfo, 128, 128) == 1);
-    }
-
-    function isSpecial(uint256 _tokenId) public view returns (bool) {
-        uint landInfo = tokenId2Attributes[_tokenId];
-        return (getInfoFromAttibutes(landInfo, 129, 129) == 1);
-    }
-
-    function hasBox(uint256 _tokenId) public view returns (bool) {
-        uint landInfo = tokenId2Attributes[_tokenId];
-        return (getInfoFromAttibutes(landInfo, 130, 130) == 1);
+        goldRate = getInfoFromAttibutes(landInfo, 0, 15);
+        woodRate = getInfoFromAttibutes(landInfo, 16, 31);
+        waterRate = getInfoFromAttibutes(landInfo, 32, 47);
+        fireRate = getInfoFromAttibutes(landInfo, 48, 63);
+        soilRate = getInfoFromAttibutes(landInfo, 64, 79);
+        flag = getInfoFromAttibutes(landInfo, 80, 95);
     }
 
 
@@ -157,40 +94,6 @@ contract LandData is Ownable {
     function changeRewardBox(address _rewardBox) public onlyOwner {
         rewardBox = _rewardBox;
     }
-
-    // helper
-    // encodeXYPositionForInfo is only used for computing
-    // it stands landInfo[0,31]
-    function encodeXYPositionForInfo(int _x, int _y) pure public returns (uint) {
-        return _unsafeEncodeTokenId(_x, _y);
-    }
-
-    function _encodeTokenId(int _x, int _y) pure internal returns (uint) {
-        return _unsafeEncodeTokenId(_x, _y);
-    }
-
-    function _unsafeEncodeTokenId(int _x, int _y) pure internal returns (uint) {
-        return ((uint(_x) * FACTOR) & CLEAR_LOW) | (uint(_y) & CLEAR_HIGH);
-    }
-
-    function _decodeTokenId(uint _value) pure internal returns (int16 x, int16 y) {
-        (x, y) = _unsafeDecodeTokenId(_value);
-    }
-
-    function _unsafeDecodeTokenId(uint _value) pure internal returns (int16 x, int16 y) {
-        x = expandNegative128BitCast((_value & CLEAR_LOW) >> 16);
-        y = expandNegative128BitCast(_value & CLEAR_HIGH);
-    }
-
-    function expandNegative128BitCast(uint _value) pure internal returns (int16) {
-        if (_value & (1 << 15) != 0) {
-            return int16(_value | CLEAR_LOW);
-        }
-        return int16(_value);
-    }
-
-
-
 
 
 
