19df90b53a1e8240f68a6e3b42800d0bea7a08d4
HackFisher <hackfisher@gmail.com>
2018-10-30 00:52:59 +0800

    add MysteriousTreasureAuthority

---
+++ contracts/MysteriousTreasureAuthority.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.24;
+
+contract MysteriousTreasureAuthority {
+
+    constructor(address[] _whitelists) public {
+        for (uint i = 0; i < _whitelists.length; i ++) {
+            whiteList[_whitelists[i]] = true;
+        }
+    }
+
+    mapping (address => bool) public whiteList;
+
+    function canCall(
+        address _src, address _dst, bytes4 _sig
+    ) public view returns (bool) {
+        return whiteList[_src];
+    }
+}
