9fab779b15453f129089ff503da0c7cc1aa845a4
HackFisher <hackfisher@gmail.com>
2018-12-19 17:58:43 +0800

    update package and remove not used codes

--- contracts/LandSettingIds.sol
+++ contracts/LandSettingIds.sol
@@ -1,16 +1,7 @@
 pragma solidity ^0.4.24;
 
 import "@evolutionland/common/contracts/SettingIds.sol";
-import "@evolutionland/common/contracts/interfaces/ISettingsRegistry.sol";
 
 contract LandSettingIds is SettingIds {
-
-    uint256 public constant GOLD_MAGNITUDE = 1;
-    uint256 public constant WOOD_MAGNITUDE = 1;
-    uint256 public constant WATER_MAGNITUDE = 1;
-    uint256 public constant FIRE_MAGNITUDE = 1;
-    uint256 public constant SOIL_MAGNITUDE = 1;
-
-    bytes32 public constant CONTRACT_RESOURCE_MINER = "CONTRACT_RESOURCE_MINER";
 
 }
