11354dda96f946c39208e217ffe55f6a5e3c8dee
echo <hujw77@gmail.com>
2021-01-21 15:38:44 +0800

    pgl: fix location range

--- flat/LandBase.sol
+++ flat/LandBase.sol
@@ -533,7 +533,7 @@ contract LandBase is DSAuth, ILandBase, SettingIds {
     }
 
     modifier xAtlantisRangeLimit(int _x) {
-        require(_x >= -22 && _x <= -22, "Invalid range.");
+        require(_x >= -22 && _x <= 22, "Invalid range.");
         _;
     }
 
