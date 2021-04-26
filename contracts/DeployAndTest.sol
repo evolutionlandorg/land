f4a70607142c34db3c476561b57dcc945013f02e
echo <hujw77@gmail.com>
2021-04-26 10:12:29 +0800

    rm outdate proxy

--- contracts/DeployAndTest.sol
+++ contracts/DeployAndTest.sol
@@ -5,7 +5,7 @@ import '@evolutionland/common/contracts/SettingsRegistry.sol';
 import '@evolutionland/common/contracts/SettingIds.sol';
 import '@evolutionland/common/contracts/StandardERC223.sol';
 import '@evolutionland/common/contracts/ObjectOwnership.sol';
-import "@evolutionland/upgraeability-using-unstructured-storage/contracts/OwnedUpgradeabilityProxy.sol";
+// import "@evolutionland/upgraeability-using-unstructured-storage/contracts/OwnedUpgradeabilityProxy.sol";
 import "@evolutionland/common/contracts/TokenLocation.sol";
 import '@evolutionland/common/contracts/ObjectOwnershipAuthority.sol';
 import "@evolutionland/common/contracts/TokenLocationAuthority.sol";
